374a3e0c93cdabf5a2966629856842fd51912769
gearloan <johnsfuller@gmail.com>
2018-06-21 12:26:22 -0400

    adding markup & styling

--- app/assets/javascripts/design_explorations.js
+++ app/assets/javascripts/design_explorations.js
@@ -2,8 +2,12 @@ $( window ).load(function() {
 
   window.onscroll = function (e) {
     //alert(0);
-    $(".guides-cover .left").addClass("slide-up hide");
-    $(".guides-cover .right").addClass("slide-down hide");
+    $(".guides-cover .left").addClass("slide-up");
+    $(".guides-cover .right").addClass("slide-down");
+    setTimeout(function(){
+      $(".guides-cover").hide();
+    }, 420);
+
   }
 
 });
